9d6becc08848360b985d8d6066350560f7a4936f
Abhinav Bora <abhinavasd2005@outlook.com>
2026-05-31 13:47:46 +0530

    Finally ready

--- src/main/java/com/finflow/security/SecurityConfig.java
+++ src/main/java/com/finflow/security/SecurityConfig.java
@@ -45,7 +45,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
+                .cors(cors -> {})   // ADD THIS
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
