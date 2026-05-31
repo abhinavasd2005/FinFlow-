046cb09aecb72f2d1f79cf8cafa711e318b29359
Abhinav Bora <abhinavasd2005@outlook.com>
2026-05-31 13:05:55 +0530

    updated api url for production

--- finflow-frontend/js/auth.js
+++ finflow-frontend/js/auth.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8080/api';
+const API_BASE = 'https://finflow-backendapp.onrender.com/api';
 
 const currentPage =
     window.location.pathname
--- finflow-frontend/js/history.js
+++ finflow-frontend/js/history.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'https://finflow-backendapp.onrender.com/api';
 
 const token = sessionStorage.getItem('token');
 const username = sessionStorage.getItem('username');
