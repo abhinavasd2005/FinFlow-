5c7febdb207c98c4726d4267929725852ee52379
Abhinav Bora <abhinavasd2005@outlook.com>
2026-09-11 20:34:52 +0530

    done

--- finflow-frontend/js/config.js
+++ finflow-frontend/js/config.js
@@ -3,4 +3,4 @@ const apiHost = window.location.hostname || 'localhost';
 
 window.FINFLOW_API_BASE = window.location.protocol === 'file:' || localHosts.has(window.location.hostname)
     ? `http://${apiHost}:10000/api`
-    : 'https://finflow-backendapp.onrender.com/api';
+    : 'https://finflow-backend-bvg9.onrender.com';
