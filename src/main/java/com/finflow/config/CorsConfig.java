5c2bd3180a570701250fa8d890aa951a5648780a
Abhinav Bora <abhinavasd2005@outlook.com>
2026-09-11 20:57:21 +0530

    done

--- src/main/java/com/finflow/config/CorsConfig.java
+++ src/main/java/com/finflow/config/CorsConfig.java
@@ -19,7 +19,6 @@ public class CorsConfig {
         config.setAllowCredentials(true);
 
         config.setAllowedOrigins(List.of(
-                "https://finflow-frontend-m169.onrender.com",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://localhost:5173",
